d31275e155611bb1f297ca05e596ed9c93d25716
diegolupi93 <diegolupi93@gmail.com>
2022-11-14 17:07:13 -0300

    add test cases

--- clases/clases.py
+++ clases/clases.py
@@ -3,6 +3,43 @@ import math
 
 class Circle:
     """ Circle Class
+
+    Test of Circle
+    repr
+    >>> print(Circle(1))
+    area: 3.141592653589793 perimeter:6.283185307179586
+
+    Change radius
+    >>> circle = Circle(1) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    <BLANKLINE>
+    ...
+    >>> circle.radius = 10 # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    <BLANKLINE>
+    ...
+    >>> print(circle)
+    area: 314.1592653589793 perimeter:62.83185307179586
+
+    Multiply
+    >>> circle = Circle(1) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    <BLANKLINE>
+    ...
+    >>> other_circle = circle * 10 # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    <BLANKLINE>
+    ...
+    >>> print(other_circle)
+    area: 314.1592653589793 perimeter:62.83185307179586
+
+    Bad Request number smaller than 0
+    >>> circle = Circle(0) # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    ...
+    ValueError: The radius should be a number bigger than 0
+
+    Bad Request not number
+    >>> circle = Circle('asd') # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    ...
+    ValueError: The radius should be a number bigger than 0
     """
     def __init__(self, value=1):
         self._validate_value(value)
@@ -35,3 +72,6 @@ class Circle:
         self._validate_value(value)
         return Circle(self._radius * value)
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
