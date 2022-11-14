24f45190251b64bd14df2e5c8cf7515546cf9428
diegolupi93 <diegolupi93@gmail.com>
2022-11-14 17:13:49 -0300

    add tests

--- matriz/matriz.py
+++ matriz/matriz.py
@@ -76,13 +76,29 @@ def getSequences(matriz, l_sequence):
     :rtype: list
 
     Test of getSequences
-    Get a sequence
-    >>> getSequences([[2, 1, 5, 5, 3], [2, 2, 5, 5, 3], [3, 3, 1, 2, 2], [5, 4, 4, 1, 2]], 4) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    Get a sequence vertical
+    >>> getSequences([[2, 1, 5, 5, 3], [2, 2, 5, 5, 3], [3, 3, 1, 2, 2], [5, 4, 4, 1, 2], [1, 1, 1, 1, 1]], 4) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
     <BLANKLINE>
     ...[((0, 1), (4, 1))]
 
+    Get a sequence vertical in reverse
+    >>> getSequences([[2, 4, 5, 5, 3], [2, 3, 5, 5, 3], [3, 2, 1, 2, 2], [5, 1, 4, 1, 2], [1, 1, 1, 1, 1]], 4) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    <BLANKLINE>
+    ...[((0, 1), (4, 1))]
+
+    Get a sequence horizontal
+    >>> getSequences([[1, 2, 3, 4, 3], [2, 3, 5, 5, 3], [3, 2, 1, 2, 2], [5, 1, 4, 1, 2], [1, 1, 1, 1, 1]], 4) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    <BLANKLINE>
+    ...[((0, 0), (0, 4))]
+
+    Get a sequence horizontal in reverse
+    >>> getSequences([[4, 3, 2, 1, 3], [2, 3, 5, 5, 3], [3, 2, 1, 2, 2], [5, 1, 4, 1, 2], [1, 1, 1, 1, 1]], 4) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    <BLANKLINE>
+    ...[((0, 0), (0, 4))]
+
+
     Do not get a sequence
-    >>> getSequences([[2, 2, 5, 5, 3], [2, 2, 5, 5, 3], [3, 3, 1, 2, 2], [5, 4, 4, 1, 2]], 4) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    >>> getSequences([[2, 2, 5, 5, 3], [2, 2, 5, 5, 3], [3, 3, 1, 2, 2], [5, 4, 4, 1, 2], [1, 1, 1, 1, 1]], 4) # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
     <BLANKLINE>
     ...[]
     """
